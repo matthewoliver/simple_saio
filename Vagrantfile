6c542a5d67354fedc8d6a563c1a743474e2feaa0
Matthew Oliver <matt@oliver.net.au>
2018-03-23 11:31:15 +1100

    Changed to 4 saios

--- Vagrantfile
+++ Vagrantfile
@@ -1,19 +1,28 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  #config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "opensuse/Tumbleweed.x86_64"
   config.ssh.insert_key = false
-  config.vm.synced_folder ".", "/vagrant", type: "virtualbox"
-  VMS = 1
+  #config.vm.synced_folder ".", "/vagrant", type: "virtualbox"
+  VMS = 4
   (0..VMS-1).each do |vm|
-    config.vm.define "server#{vm}" do |g|
-        g.vm.hostname = "server#{vm}"
+    config.vm.define "suse-saio#{vm}" do |g|
+        g.vm.hostname = "suse-saio#{vm}"
+    #config.vm.define "saio#{vm}" do |g|
+    #    g.vm.hostname = "saio#{vm}"
         g.vm.network :private_network, type: "dhcp"
         g.vm.provider :virtualbox do |vb|
             vb.memory = 2048
             vb.cpus = 2
         end
+        g.vm.provider :libvirt do |v|
+             v.memory = 2048
+             v.cpus = 2
+        end
         config.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/me.pub"
         config.vm.provision "file", source: "saio.sh", destination: "~/saio.sh"
         config.vm.provision "shell", inline: "cat ~vagrant/.ssh/me.pub >> ~vagrant/.ssh/authorized_keys"
+        config.vm.provision "file", source: "~/.ssh/id_rsa_vagrant", destination: "~/.ssh/id_rsa"
+        config.vm.provision "file", source: "~/.ssh/id_rsa_vagrant.pub", destination: "~/.ssh/id_rsa.pub"
         config.vm.provision "shell", inline: "bash ~vagrant/saio.sh", privileged: false
     end
   end
--- saio.sh
+++ saio.sh
@@ -5,9 +5,10 @@ SUSE=2
 SIZE=1
 UNIT_TESTS=1
 BUILD_LIBERASURECODE=1
+SETUP_VENV=1
 branch=${1:-master}
 
-DISTRO=$DEBIAN
+DISTRO=$SUSE
 
 function build_liberasurecode() {
   if [ ${BUILD_LIBERASURECODE} -eq 0 ]
@@ -23,8 +24,11 @@ function build_liberasurecode() {
   if [ $DISTRO -eq $DEBIAN ]
   then
     sudo apt-get install -y build-essential autoconf automake libtool
-  else
+  elif [ $DISTRO -eq $REDHAT ]
+  then
     sudo yum install -y gcc make autoconf automake libtool
+  else
+    sudo zypper --gpg-auto-import-keys install -y gcc make autoconf automake libtool
   fi
 
   ./autogen.sh
@@ -47,8 +51,11 @@ then
                        python-greenlet python-pastedeploy \
                        python-netifaces python-pip python-dnspython \
                        python-mock
-else
+  users_grp="${USER}"
+elif [ $DISTRO -eq $REDHAT ]
+then
   sudo yum update
+  sudo yum install -y epel-release
   sudo yum install -y curl gcc memcached rsync sqlite xfsprogs git-core \
                       libffi-devel xinetd \
                       openssl-devel python-setuptools \
@@ -57,6 +64,21 @@ else
                       python-greenlet python-paste-deploy \
                       python-netifaces python-pip python-dns \
                       python-mock
+  users_grp="${USER}"
+else
+  sudo zypper --gpg-auto-import-keys install -y \
+                      curl gcc memcached rsync sqlite3 xfsprogs git-core \
+                      libffi-devel liberasurecode-devel python2-setuptools \
+                      libopenssl-devel zlib-devel python3
+  sudo zypper --gpg-auto-import-keys install -y \
+											python2-coverage python2-devel python2-nose \
+                      python2-xattr python2-greenlet \
+                      python2-netifaces python2-pip python2-dnspython \
+                      python3-coverage python3-devel python3-nose \
+                      python3-xattr python3-eventlet python3-greenlet \
+                      python3-netifaces python3-pip python3-dnspython \
+                      python3-mock python3-dbm
+  users_grp="users"
 fi
 
 if [ $BUILD_LIBERASURECODE -gt 0 ]
@@ -66,38 +88,43 @@ else
   if [ $DISTRO -eq $DEBIAN ]
   then
     sudo apt-get install -y liberasurecode-dev
-  else
+  elif [ $DISTRO -eq $REDHAT ]
+  then
     sudo yum install -y liberasurecode-devel
+  else
+    sudo zypper install -y liberasurecode-devel
   fi
 fi
 
 # Using loopback device
-sudo mkdir /srv
+sudo mkdir -p /srv
 sudo truncate -s $(echo $SIZE)GB /srv/swift-disk
 sudo mkfs.xfs /srv/swift-disk
 
 # Add to fstab
 if [ $(grep -c "/srv/swift-disk" /etc/fstab) -gt 0 ]
 then
-    sudo sed -i 's|^/srv/swift-disk .*$|/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,nobarrier,logbufs=8 0 0|' /etc/fstab
+    #sudo sed -i 's|^/srv/swift-disk .*$|/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,nobarrier,logbufs=8 0 0|' /etc/fstab
+    sudo sed -i 's|^/srv/swift-disk .*$|/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,logbufs=8 0 0|' /etc/fstab
 else
-    sudo su -c "echo '/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,nobarrier,logbufs=8 0 0' >> /etc/fstab"
+    #sudo su -c "echo '/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,nobarrier,logbufs=8 0 0' >> /etc/fstab"
+    sudo su -c "echo '/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,logbufs=8 0 0' >> /etc/fstab"
 fi
 
 # create the mountpoints 
 sudo mkdir -p /mnt/sdb1
 sudo mount /mnt/sdb1
 sudo mkdir -p /mnt/sdb1/1 /mnt/sdb1/2 /mnt/sdb1/3 /mnt/sdb1/4
-sudo chown ${USER}:${USER} /mnt/sdb1/*
+sudo chown ${USER}:${users_grp} /mnt/sdb1/*
 for x in {1..4}; do sudo ln -s /mnt/sdb1/$x /srv/$x; done
 sudo mkdir -p /srv/1/node/sdb1 /srv/1/node/sdb5 \
               /srv/2/node/sdb2 /srv/2/node/sdb6 \
               /srv/3/node/sdb3 /srv/3/node/sdb7 \
               /srv/4/node/sdb4 /srv/4/node/sdb8 \
               /var/run/swift
-sudo chown -R ${USER}:${USER} /var/run/swift
+sudo chown -R ${USER}:${users_grp} /var/run/swift
 # **Make sure to include the trailing slash after /srv/$x/**
-for x in {1..4}; do sudo chown -R ${USER}:${USER} /srv/$x/; done
+for x in {1..4}; do sudo chown -R ${USER}:${users_grp} /srv/$x/; done
 
 setup_saio_mounts=/usr/local/bin/setup_saio_mounts.sh
 sudo mkdir -p $(dirname $setup_saio_mounts)
@@ -105,49 +132,63 @@ sudo touch $setup_saio_mounts
 sudo chmod +x $setup_saio_mounts
 cat << EOF |sudo tee $setup_saio_mounts
 mkdir -p /var/cache/swift /var/cache/swift2 /var/cache/swift3 /var/cache/swift4
-chown ${USER}:${USER} /var/cache/swift*
+chown ${USER}:${users_grp} /var/cache/swift*
 mkdir -p /var/run/swift
-chown ${USER}:${USER} /var/run/swift
+chown ${USER}:${users_grp} /var/run/swift
 EOF
 sudo $setup_saio_mounts
 
 if [ $DISTRO -eq $DEBIAN ]
 then
-  if [ -e /etc/rc.local ] && [ $(grep -c "$setup_saio_mounts" /etc/rc.local) -eq 0 ]
-  then
-    if [$(grep -c "^exit 0" /etc/rc.local) -gt 0]
-    then
-      sudo sed -i "/exit 0/i$setup_saio_mounts\n" /etc/rc.local
-    else
-      echo "$setup_saio_mounts" | sudo tee --append /etc/rc.local
-    fi
-  fi
+  rc_local="/etc/rc.local"
 elif [ $DISTRO -eq $REDHAT ]
 then
-  if [ -e /etc/rc.d/rc.local ] && [ $(grep -c "$setup_saio_mounts" /etc/rc.d/rc.local) -eq 0 ]
-  then
-    if [$(grep -c "^exit 0" /etc/rc.d/rc.local) -gt 0]
-    then
-      sudo sed -i "/exit 0/i$setup_saio_mounts\n" /etc/rc.d/rc.local
-    else
-      echo "$setup_saio_mounts" |sudo tee --append /etc/rc.d/rc.local
-    fi
-  fi
+  rc_local="/etc/rc.d/rc.local"
+else
+  rc_local="/etc/init.d/boot.local"
 fi 
+if [ -e $rc_local ] && [ $(grep -c "$setup_saio_mounts" $rc_local) -eq 0 ]
+then
+  if [ $(grep -c "^exit 0" $rc_local) -gt 0 ]
+  then
+    sudo sed -i "/exit 0/i$setup_saio_mounts\n" $rc_local
+  else
+    echo "$setup_saio_mounts" |sudo tee --append $rc_local
+  fi
+fi
 
 # upgrade pip and tox
 sudo pip install pip tox setuptools --upgrade
 
+if [ $SETUP_VENV -gt 0 ]
+then
+  mkdir -p  $HOME/venv
+  virtualenv $HOME/venv
+  source $HOME/venv/bin/activate
+fi
+
 # get the code
 cd $HOME; git clone https://github.com/openstack/python-swiftclient.git
-cd $HOME/python-swiftclient; sudo pip install -e . ; cd -
+if [ $SETUP_VENV -gt 0 ]
+then
+  PIP="pip"
+else
+  PIP="sudo pip"
+fi
+cd $HOME/python-swiftclient; $PIP install -e . ; cd -
 
 git clone https://github.com/openstack/swift.git
-cd $HOME/swift; git checkout $branch; sudo pip install --no-binary cryptography -r requirements.txt; sudo pip install -e . ; cd -
+cd $HOME/swift; git checkout $branch; sudo pip install --no-binary cryptography -r requirements.txt;
+$PIP install -e .
+cd -
 
 if [ $DISTRO -eq $REDHAT ]
 then
-  sudo pip install -U xattr
+  $PIP install -U xattr
+elif
+  [ $DISTRO -eq $SUSE ]
+then
+  $PIP install ipaddress
 fi
 
 sudo pip install -r swift/test-requirements.txt
@@ -159,10 +200,14 @@ if [ $DISTRO -eq $DEBIAN ]
 then
   sudo sed -i 's/^RSYNC_ENABLE=.*$/RSYNC_ENABLE=true/' /etc/default/rsync
   sudo service rsync restart
-else
+elif [ $DISTRO -eq $REDHAT ]
+then
   sudo sed -i 's/^disable =.*$/disable = no/' /etc/xinetd.d/rsync
   sudo setenforce Permissive
   sudo systemctl restart xinetd.service
+  sudo systemctl enable rsyncd.service
+  sudo systemctl start rsyncd.service
+else
   sudo systemctl enable rsyncd.service
   sudo systemctl start rsyncd.service
 fi
@@ -183,12 +228,13 @@ sudo service rsyslog restart
 sudo rm -rf /etc/swift
 
 cd $HOME/swift/doc; sudo cp -r saio/swift /etc/swift; cd -
-sudo chown -R ${USER}:${USER} /etc/swift
+sudo chown -R ${USER}:${users_grp} /etc/swift
 
 find /etc/swift/ -name \*.conf | xargs sudo sed -i "s/<your-user-name>/${USER}/"
 
 # setup scripts for runnning swift
-cd $HOME/swift/doc; cp -r saio/bin $HOME/bin; cd -
+mkdir -p $HOME/bin
+cd $HOME/swift/doc; cp -r saio/bin/* $HOME/bin/; cd -
 chmod +x $HOME/bin/*
 
 # We are using loopback so...
